6cc1afe5819f20eb7b4f711ad3bbeb4855301753
23DP3KCeri <166131730+23DP3KCeri@users.noreply.github.com>
2024-10-01 17:54:08 +0000

    Counting

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,11 +1,19 @@
 package lv.rvt;
 
+import java.util.Scanner;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        // Make your first Hello World app!
-
+    public class Counting {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+    
+            int number = scanner.nextInt();
+    
+            for (int i = 0; i <= number; i++) {
+                System.out.println(i);
+            }
+            scanner.close();
+        }
     }
-
 }
