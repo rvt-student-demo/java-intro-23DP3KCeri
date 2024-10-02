0ab0214a45208c53d2789f01297eb138f54e4a49
23DP3KCeri <166131730+23DP3KCeri@users.noreply.github.com>
2024-10-02 07:09:34 +0000

    Repeating, breaking and remembering

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -16,34 +16,27 @@ public static void main(String[] args) {
     
             while (true) {
                 int num = scanner.nextInt();
-                // If user inputs -1, break the loop
                 if (num == -1) {
                     System.out.println("Thx! Bye!");
     
-                    // Part 2: Sum of numbers
                     System.out.println("Sum: " + sum);
     
-                    // Part 3: Number of numbers entered (excluding -1)
                     System.out.println("Numbers: " + count);
     
-                    // Part 4: Average of the numbers (excluding -1)
                     if (count > 0) {
                         double average = (double) sum / count;
                         System.out.println("Average: " + average);
                     }
     
-                    // Part 5: Even and Odd numbers
                     System.out.println("Even: " + evenCount);
                     System.out.println("Odd: " + oddCount);
     
                     break;
                 }
     
-                // Update the sum and count
                 sum += num;
                 count++;
     
-                // Check if the number is even or odd
                 if (num % 2 == 0) {
                     evenCount++;
                 } else {
