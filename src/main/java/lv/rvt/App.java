2a1be4acbf11f8da94af6433da8b7d4e0aaea67c
23DP3KCeri <166131730+23DP3KCeri@users.noreply.github.com>
2024-10-02 06:56:24 +0000

    Repeating, breaking and remembering

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,24 +5,55 @@ import java.util.Scanner;
 public class App 
 { 
 
-public class SumOfSequence {
-    public static void main(String[] args) {
-        
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Last number? ");
-        int number = scanner.nextInt();
-
-        int sum = 0;
-
-        
-        for (int i = 1; i <= number; i++) {
-            sum += i;
+public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            int sum = 0;
+            int count = 0;
+            int evenCount = 0;
+            int oddCount = 0;
+    
+            System.out.println("Write numbers:");
+    
+            while (true) {
+                int num = scanner.nextInt();
+                // If user inputs -1, break the loop
+                if (num == -1) {
+                    System.out.println("Thx! Bye!");
+    
+                    // Part 2: Sum of numbers
+                    System.out.println("Sum: " + sum);
+    
+                    // Part 3: Number of numbers entered (excluding -1)
+                    System.out.println("Numbers: " + count);
+    
+                    // Part 4: Average of the numbers (excluding -1)
+                    if (count > 0) {
+                        double average = (double) sum / count;
+                        System.out.println("Average: " + average);
+                    }
+    
+                    // Part 5: Even and Odd numbers
+                    System.out.println("Even: " + evenCount);
+                    System.out.println("Odd: " + oddCount);
+    
+                    break;
+                }
+    
+                // Update the sum and count
+                sum += num;
+                count++;
+    
+                // Check if the number is even or odd
+                if (num % 2 == 0) {
+                    evenCount++;
+                } else {
+                    oddCount++;
+                }
+            }
+    
+            scanner.close();
         }
+    } 
+    
 
-        System.out.println("The sum is " + sum);
 
-        scanner.close();
-    }
-}
-}
