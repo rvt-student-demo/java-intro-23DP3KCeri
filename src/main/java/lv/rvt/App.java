b6e9f7279a269dfd27ca5fbd9f09293c21e2391d
23DP3KCeri <166131730+23DP3KCeri@users.noreply.github.com>
2024-10-09 07:08:25 +0000

    Divisible by three

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,49 +4,26 @@ import java.util.Scanner;
 
 public class App 
 { 
-
-public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            int sum = 0;
-            int count = 0;
-            int evenCount = 0;
-            int oddCount = 0;
-    
-            System.out.println("Write numbers:");
-    
-            while (true) {
-                int num = scanner.nextInt();
-                if (num == -1) {
-                    System.out.println("Thx! Bye!");
-    
-                    System.out.println("Sum: " + sum);
-    
-                    System.out.println("Numbers: " + count);
-    
-                    if (count > 0) {
-                        double average = (double) sum / count;
-                        System.out.println("Average: " + average);
-                    }
-    
-                    System.out.println("Even: " + evenCount);
-                    System.out.println("Odd: " + oddCount);
-    
-                    break;
-                }
-    
-                sum += num;
-                count++;
-    
-                if (num % 2 == 0) {
-                    evenCount++;
-                } else {
-                    oddCount++;
-                }
-            }
-    
-            scanner.close();
+    public static void divisibleByThreeInRange(int beginning, int end) {
+        for (int i = beginning; i <= end; i++) {
+        if (i % 3 == 0) {
+        System.out.println(i);
         }
-    } 
+    }
+}
+
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Ievadiet diapazona sākumu: ");
+    int beginning = scanner.nextInt();
+
+    System.out.print("Ievadiet diapazona beigas: ");
+    int end = scanner.nextInt();
+
+    divisibleByThreeInRange(beginning, end);
+    }
+} 
     
 
 
