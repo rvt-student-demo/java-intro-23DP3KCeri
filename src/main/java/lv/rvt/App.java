70f7642ba7df812b7ee30467eb502b0c62c7a4c4
23DP3KCeri <166131730+23DP3KCeri@users.noreply.github.com>
2024-10-16 06:10:35 +0000

    christmastree

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,25 +1,37 @@
 package lv.rvt;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class App 
 { 
-    public static void printStars(int number) {
-        int times = 0;
-        String star = "*";
-        while (times<number){
-            System.out.print(star);
-            times += 1;
+    public static void main(String[] args) {
+        christmasTree(4);
+    }  
+
+    public static void christmasTree(int height){
+        for(int i= 1; i <= height; i++){
+            printSpaces(height - i);
+            printStars(2 * i - 1);
+            System.out.println();
         }
 
+        for(int i=0; i < 2; i++){
+            printSpaces(height - 2);
+            printStars(3);
+            System.out.println();
+        }
+    }
+
+    public static void printSpaces(int count){
+        for (int i = 0; i < count; i++){
+            System.out.print(" ");
+        }
     }
     
-    public static void main(String[] args) {
-        printStars(5);
-        System.out.println("");
-        printStars(3);
-        System.out.println("");
-        printStars(9);
+    public static void printStars(int count){
+        for (int i = 0; i < count; i++){
+            System.out.print("*");
+        }
     }
 } 
     
