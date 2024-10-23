37ebd558fe499d233bd9580597c42d6f18cc8b19
23DP3KCeri <166131730+23DP3KCeri@users.noreply.github.com>
2024-10-23 07:15:10 +0000

    Only these numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,27 +8,21 @@ public class App
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ArrayList<Integer> numbers = new ArrayList<>();
-
-        System.out.println("");
-        while(true){
-            int number = scanner.nextInt();
-            if(number==0){
-                break;
-            }
-            numbers.add(number);
+        while (true) {
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        if (number == -1) {
+        break;
         }
-        System.out.println("");
-        for (int num : numbers){
-            System.out.println(num);
+        numbers.add(number);
         }
-        int sum = 0;
-        for (int num : numbers){
-            sum+=num;
+        System.out.print("From where? ");
+        int startIndex = scanner.nextInt();
+        System.out.print("To where? ");
+        int endIndex = scanner.nextInt();
+        for (int i = startIndex; i <= endIndex; i++) {
+        System.out.println(numbers.get(i));
         }
-        System.out.println("" + sum);
-        scanner.close();
-    }
-} 
-    
-
+        }
+}
 
