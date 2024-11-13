d5ec7013da7ba1bcbe114e8d9578552433567391
23DP3KCeri <166131730+23DP3KCeri@users.noreply.github.com>
2024-11-13 08:44:13 +0000

    Payment Card

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,49 +5,23 @@ import java.util.ArrayList;
 
 public class App 
 { 
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<String> userNames = new ArrayList<String>();
-        ArrayList<Integer> userAges = new ArrayList<Integer>();
+    public static void main(String[] args) {
+        PaymentCard paulsCard = new PaymentCard(20);
+        PaymentCard mattsCard = new PaymentCard(30);
 
-        while (true) {
-            String userInput = scanner.nextLine().trim();
-
-            if (userInput.length() > 0){
-                String[] splitStringArray = userInput.split(",");
-                userNames.add(splitStringArray[0]);
-                userAges.add(Integer.valueOf(splitStringArray[1]));
-            }
-
-            if (userInput.equals("")){
-                break;
-            }
-        }
-
-        int sumOfBirthYears = 0;
-        int numOfYears = 0;
-
-        int longestString = userNames.get(0).length();
-        int index = 0;
-
-        for (int i = 0; i < userNames.size(); i++) {
-
-            if (userNames.get(i).length() > longestString) {
-
-                longestString = userNames.get(i).length();
-                index = i;
-
-            }
-            sumOfBirthYears += userAges.get(i);
-            numOfYears++;
-
-        }
-        double average = 1.0 * sumOfBirthYears / numOfYears;
-
-        System.out.println("Longest name: " + userNames.get(index));
-
-        System.out.println("Average of the birth years: " + average);
+        paulsCard.eatHeartily();
+        mattsCard.eatAffordably();
+        System.out.println("Paul:" + paulsCard);
+        System.out.println(" Matt: " + mattsCard);
+        paulsCard.addMoney(20);
+        mattsCard.eatHeartily();
+        System.out.println("Paul:" + paulsCard);
+        System.out.println(" Matt: " + mattsCard);
+        paulsCard.eatAffordably();
+        paulsCard.eatAffordably();
+        mattsCard.addMoney(50);
+        System.out.println("Paul:" + paulsCard);
+        System.out.println(" Matt: " + mattsCard);
     }
-           
 }
 
