37ed80dc7cd6fd8d1011ed7e073d90834f4cf6f6
23DP3KCeri <166131730+23DP3KCeri@users.noreply.github.com>
2024-11-22 09:04:49 +0000

    One Minute

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,48 +5,20 @@ import java.util.ArrayList;
 
 public class App{
     public static void main(String[] args) {
-
-        // implement here the program that allows the user to enter 
-        // book information and to examine them
-        ArrayList<Books> books = new ArrayList<>();
-
-        Scanner scanner = new Scanner(System.in);
+       
+        Timer timer = new Timer();
+        
+        
 
         while (true) {
-            System.out.println("Title: ");
-            String inputTitle = scanner.nextLine();
-            if (inputTitle.isEmpty()) {
-                break;
+            System.out.println(timer);
+            timer.advance();
+
+            try {
+                Thread.sleep(3);
+            } catch (Exception e) {
+
             }
-
-            System.out.println("Publication year: ");
-
-            int inputPubYear = Integer.valueOf(scanner.nextLine());
-
-            System.out.println("Pages: ");
-
-            int inputPages = Integer.valueOf(scanner.nextLine());
-
-            books.add(new Books(inputTitle, inputPages, inputPubYear));
-        }
-
-        System.out.println("What information will be printed? ");
-
-        String whatToPrint = scanner.nextLine();
-
-        if (whatToPrint.toLowerCase().equals("everything")) {
-            for (Books element : books) {
-                System.out.println(element);
-            }
-        }
-        if (whatToPrint.toLowerCase().equals("name")) {
-            for (Books element : books) {
-                System.out.println(element.getTitle());
-            }
-            
-        } 
-        else {
-           return;
         }
 
     }
